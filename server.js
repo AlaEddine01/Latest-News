af0e90fcaed9cf27e8cfd9572b9ba5a520eb424f
AlaEddine01 <alaeddinebrahem@yahoo.fr>
2022-03-02 16:07:11 +0100

    fix the port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
 }
 // -------------------------------------------
 
-app.listen(port, (err) => {
+app.listen(process.env.PORT, (err) => {
   if (err) console.log("server not connected");
   else console.log("server is connected on port: " + port);
 });
