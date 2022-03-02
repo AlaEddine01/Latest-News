49663005b174063e2e9b9440a560cecf6ce6c43d
AlaEddine01 <alaeddinebrahem@yahoo.fr>
2022-03-02 15:24:50 +0100

    deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
 const passport = require("passport");
-// const path = require("path");
+const path = require("path");
 
 //app config
 const app = express();
@@ -15,13 +15,21 @@ app.use(cors());
 //  Import passport MiddleWare
 require("./middleware/passport")(passport);
 
-
 //  Connect Data Base
 connectDB();
 
 //  API Call
 app.use("/articles", require("./routes/article"));
 app.use("/users", require("./routes/article"));
+
+// ------------------Deployment---------------
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+// -------------------------------------------
 
 app.listen(PORT, (err) => {
   if (err) console.log("server not connected");
